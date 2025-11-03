15d13b1a500b7651eaa196b1a3299f1ee2a169b0
Eder Rosa <eder_junior07@hotmail.com>
2025-11-03 17:02:09 -0300

    up

--- src/components/new-lp-partners/index.tsx
+++ src/components/new-lp-partners/index.tsx
@@ -84,18 +84,21 @@ const NewLpPartners = ({
   const t = useTranslations('partners')
 
   return (
-    <section className="overflow-hidden lg:flex-row flex-col-reverse px-4 flex items-center justify-center max-w-7xl mx-auto">
+    <section className="overflow-hidden mt-3 lg:mt-0 lg:flex-row flex-col-reverse px-4 flex items-center justify-center max-w-7xl mx-auto">
       <div className="flex flex-col">
-        <h2 className="mb-6 border-l-4 border-cyan-400 pl-4 text-pretty text-black text-2xl font-bold lg:text-4xl">
+        <h2 className="lg:mb-6 mb-3 text-center text-balance text-black text-3xl font-bold lg:text-4xl">
           {t('title')}
         </h2>
-        <p className="text-balance text-base text-zinc-500 lg:text-xl">
+        <div className="relative mb-3 mx-auto lg:hidden block w-[300px] h-[100px]">
+           <Image src="/thomson-removebg-preview.PNG" alt="Partners" fill className="object-cover" />
+        </div>
+        <p className="text-balance text-center lg:text-left text-base text-zinc-500 lg:text-xl">
           {t('description')}
         </p>
       </div>
       <div className="flex items-center justify-center">
-        <div className="relative lg:w-[400px] lg:h-[400px] w-[132px] h-[132px]">
-        <Image src="/thomson.PNG" alt="Partners" fill className="object-contain" />
+        <div className="relative hidden lg:block lg:w-[400px] lg:h-[400px] w-[132px] h-[132px]">
+           <Image src="/thomson-removebg-preview.PNG" alt="Partners" fill className="object-contain" />
         </div>
       </div>
       {/* <div>
